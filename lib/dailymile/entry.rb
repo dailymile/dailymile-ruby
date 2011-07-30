92c996e94b6870d9a76f189310410f5be2d32b22
Felix Tjandrawibawa <cemeng@gmail.com>
2011-07-30 20:55:04 +1000

    Code revisions and adding test (Sydney RORO Hacknight)

--- lib/dailymile/entry.rb
+++ lib/dailymile/entry.rb
@@ -2,86 +2,78 @@ module Dailymile
 
   class Entry
 
-		attr_reader :message, :lat, :lon
-		attr_writer :message, :lat, :lon
+		attr_accessor :message, :lat, :lon
 
 		ACTIVITY_TYPES 	= [ "running", "cycling", "swimming", "walking", "fitness" ]
 		DISTANCE_UNITS 	= [ "miles", "kilometers", "yards", "meters" ]
 		FEELINGS 				= [ "great", "good", "alright", "blah", "tired", "injured"] 
 
-		def initialize(message=nil)
-			@entry={}
-			@entry['workout']={}
-			@entry['message']=message
+		def initialize(attributes={})
+			@attributes = attributes
+			@attributes['workout'] ||= {}
 		end
 
 		def activity_type=(activity_type)
 			unless ACTIVITY_TYPES.include?( activity_type )
-				#TODO raise a proper exception
-				raise 'WorkoutException: unknown activity type ' + activity_type
+				raise DailymileError, 'WorkoutException: unknown activity type ' + activity_type
 			else
-				@entry['workout'][ 'activity_type' ] = activity_type
+				@attributes['workout'][ 'activity_type' ] = activity_type
 			end 
 		end
 
 		# TODO: Need to do datevtype checking here
 		def completed_at=(datetime)
-			@entry['workout'][ 'completed_at' ] = DateTime.parse( datetime.to_time.getutc.to_s ).iso8601
+			@attributes['workout'][ 'completed_at' ] = DateTime.parse( datetime.to_time.getutc.to_s ).iso8601
 		end
 
 		#TODO: how to type check that distance is hash?
 		def distance=(distance)
-			@entry['workout'][ 'distance' ]={}
-			@entry['workout'][ 'distance' ][ 'value' ] = distance[ 'value' ]
-			unless distance[ 'units' ].nil?
+			@attributes['workout'][ 'distance' ] ||= {}
+			@attributes['workout'][ 'distance' ][ 'value' ] = distance[ 'value' ]
+			if distance[ 'units' ]
 				unless DISTANCE_UNITS.include?( distance['units'] )
-					#TODO raise a proper exception
-					raise 'WorkoutException: unknown units ' + distance['units']
+					raise DailymileError, 'WorkoutException: unknown units ' + distance['units']
 				else			
-					@entry['workout'][ 'distance' ][ 'units' ] = distance['units']
+					@attributes['workout'][ 'distance' ][ 'units' ] = distance['units']
 				end 
 			end
 		end
 				
 		def duration=(duration)
-			@entry['workout'][ 'duration' ] = duration
+			@attributes['workout'][ 'duration' ] = duration
 		end
 		
 		def felt=(felt)
 			unless FEELINGS.include?( felt )
 				#TODO raise a proper exception
-				raise 'WorkoutException: unknown feeling ' + felt
+				raise DailymileError, 'WorkoutException: unknown feeling ' + felt
 			else
-				@entry['workout'][ 'felt' ] = felt
+				@attributes['workout'][ 'felt' ] = felt
 			end
 		end
 		
 		def calories=(calories)
-			@entry['workout'][ 'calories' ] = calories
+			@attributes['workout'][ 'calories' ] = calories
 		end
 		
 		def title=(title)
-			@entry['workout'][ 'title' ] = title
+			@attributes['workout'][ 'title' ] = title
 		end
 		
 		def route_id=(route_id)
-			@entry['workout'][ 'route_id' ] = route_id
-		end
-		
-		def get_entry
-		 
+			@attributes['workout'][ 'route_id' ] = route_id
 		end
 		
 		def message=(message)
-			@entry[ 'message' ] = message
+			@attributes[ 'message' ] = message
 		end
 
 		def workout
-			@entry['workout']
+			@attributes['workout']
 		end 
 		
 		def entry
-			@entry
+			@attributes
 		end   
   end
 end
--- test/test_dailymile-ruby.rb
+++ test/test_dailymile-ruby.rb
@@ -1,6 +1,63 @@
 require 'helper'
 
 class TestDailymileRuby < Test::Unit::TestCase
+  def setup
+  	WebMock.disable_net_connect!
+  	
+  	params = { :message => 'Totally awesome', 
+  		:workout => {
+  		  :activity_type 	=> 'running',
+  		  :duration 			=> 3600,
+  		  :felt 					=> 'great',
+  		  :calories 			=> 400,
+  		  :title 					=> 'API Test',
+  		  :distance				=> {
+  		    :value => 5,
+  		    :units => 'kilometers'
+  		  },
+  		  :oauth_token		=> 'XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4',
+  		  :completed_at		=> '2011-07-19 13:00'
+  		}	
+  	}
+		
+		stub_request(:post, "https://api.dailymile.com/entries.json").
+			with(:body => Rack::Utils.build_nested_query( params ), 
+				   :headers => { 'Authorization'=>'OAuth XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4' }
+			).
+			to_return(:status => 200, :body => "", :headers => {})
+		
+		
+  	
+  end
+  
+  should "be able to post an entry" do
+		Dailymile::Client.set_client_credentials 'qnFYExIvAhpsBLY4DU7w4jJerrmtBTUOQ4zccS1e', 'uE2tbUVPAjzXftUVZySLGvISEQkyeGQrSuh3Jz1n'
+		client = Dailymile::Client.new 'XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4'
+
+
+		entry = Dailymile::Entry.new :message => "Totally awesome"
+
+
+		entry.activity_type = "running" 
+		entry.duration=3600
+		entry.felt="great"
+		entry.calories=400
+		entry.title="API Test"
+
+		# Test units optional
+		distance = { "value" => 5, "units" => "kilometers" }
+		entry.distance=distance
+
+		#entry.completed_at=""
+		entry.completed_at=DateTime.parse('2011-07-19 13:00')
+
+		puts entry.entry
+
+		client.post_entry(entry.entry)  
+		
+		
+  end
+  
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
   end
