c7b31329012dec560ed451c6771400eb453a5ac8
FELIX TJANDRAWIBAWA <cemeng@FELIX-TJANDRAWIBAWAs-MacBook-Pro.local>
2011-08-03 22:05:57 +1000

    webmock test that dont work at the moment

--- test/test_dailymile-ruby.rb
+++ test/test_dailymile-ruby.rb
@@ -4,58 +4,84 @@ class TestDailymileRuby < Test::Unit::TestCase
   def setup
   	WebMock.disable_net_connect!
   	
-  	params = { :message => 'Totally awesome', 
-  		:workout => {
-  		  :activity_type 	=> 'running',
-  		  :duration 			=> 3600,
-  		  :felt 					=> 'great',
-  		  :calories 			=> 400,
-  		  :title 					=> 'API Test',
-  		  :distance				=> {
-  		    :value => 5,
-  		    :units => 'kilometers'
-  		  },
-  		  :oauth_token		=> 'XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4',
-  		  :completed_at		=> '2011-07-19 13:00'
-  		}	
-  	}
-		
-		stub_request(:post, "https://api.dailymile.com/entries.json").
-			with(:body => Rack::Utils.build_nested_query( params ), 
-				   :headers => { 'Authorization'=>'OAuth XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4' }
-			).
-			to_return(:status => 200, :body => "", :headers => {})
-		
-		
-  	
+
   end
   
-  should "be able to post an entry" do
-		Dailymile::Client.set_client_credentials 'qnFYExIvAhpsBLY4DU7w4jJerrmtBTUOQ4zccS1e', 'uE2tbUVPAjzXftUVZySLGvISEQkyeGQrSuh3Jz1n'
-		client = Dailymile::Client.new 'XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4'
+  should "be able to post a simple entry" do
+    # Stub setup
+    params = { 
+      :message => "Totally awesome", 
+      :oauth_token    => "XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4",
+      :completed_at   => "2011-07-19 13:00"
+    }
+
+    stub_request(:post, "https://api.dailymile.com/entries.json").
+      with(
+        :body => Rack::Utils.build_nested_query( params ), 
+        :headers => {'Accept'=>'application/json', 'Authorization'=>'OAuth XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4', 
+          'Content-Type'=>'application/x-www-form-urlencoded', 
+          'User-Agent'=>'dailymile-ruby/0.2.0'}
+        #:headers => { 'Authorization'=>'OAuth XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4' }
+      ).
+      to_return(:status => 200, :body => "", :headers => {}
+    )
 
 
-		entry = Dailymile::Entry.new :message => "Totally awesome"
+    # Test
+    Dailymile::Client.set_client_credentials 'qnFYExIvAhpsBLY4DU7w4jJerrmtBTUOQ4zccS1e', 'uE2tbUVPAjzXftUVZySLGvISEQkyeGQrSuh3Jz1n'
+    client = Dailymile::Client.new 'XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4'
 
+    entry = Dailymile::Entry.new :message => "Totally awesome"
+    client.post_entry(entry.entry)  
+  end
 
-		entry.activity_type = "running" 
-		entry.duration=3600
-		entry.felt="great"
-		entry.calories=400
-		entry.title="API Test"
+  should "be able to post a complete entry" do
+    # Stub setup
+    params = { :message => "Totally awesome", 
+      :workout => {
+        :activity_type  => "running",
+        :duration       => 3600,
+        :felt           => "great",
+        :calories       => 400,
+        :title          => "API Test",
+        :distance       => {
+          :value => 5,
+          :units => "kilometers"
+        },
+        :oauth_token    => "XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4",
+        :completed_at   => "2011-07-19 13:00"
+      } 
+    }
 
-		# Test units optional
-		distance = { "value" => 5, "units" => "kilometers" }
-		entry.distance=distance
+    stub_request(:post, "https://api.dailymile.com/entries.json").
+      with(
+        :body => Rack::Utils.build_nested_query( params ), 
+        :headers => {'Accept'=>'application/json', 'Authorization'=>'OAuth XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4', 
+          'Content-Type'=>'application/x-www-form-urlencoded', 
+          'User-Agent'=>'dailymile-ruby/0.2.0'}
+   
+        #:headers => { 'Authorization'=>'OAuth XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4' }
+      ).
+      to_return(:status => 200, :body => "", :headers => {}
+    )
 
-		#entry.completed_at=""
-		entry.completed_at=DateTime.parse('2011-07-19 13:00')
+    # Test
+  	Dailymile::Client.set_client_credentials 'qnFYExIvAhpsBLY4DU7w4jJerrmtBTUOQ4zccS1e', 'uE2tbUVPAjzXftUVZySLGvISEQkyeGQrSuh3Jz1n'
+  	client = Dailymile::Client.new 'XXVTcIrfN2cIs43Yg3de56LIYGsWjTXmNiRmR6H4'
 
-		puts entry.entry
+  	entry = Dailymile::Entry.new :message => "Totally awesome"
+  	entry.activity_type = "running" 
+  	entry.duration=3600
+  	entry.felt="great"
+  	entry.calories=400
+  	entry.title="API Test"
+    entry.completed_at=DateTime.parse('2011-07-19 13:00')
 
-		client.post_entry(entry.entry)  
-		
-		
+  	# Test units optional
+  	distance = { "value" => 5, "units" => "kilometers" }
+  	entry.distance=distance
+
+  	client.post_entry(entry.entry)  
   end
   
   should "probably rename this file and start testing for real" do
